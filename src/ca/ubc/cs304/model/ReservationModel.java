6bd23b1d34a03f91c388637aebe5bc3b8dc9c69b
Jasmine Zhang <jasminezhangys@gmail.com>
2019-11-24 18:06:21 -0800

    Update ReservationModel.java

--- src/ca/ubc/cs304/model/ReservationModel.java
+++ src/ca/ubc/cs304/model/ReservationModel.java
@@ -1,4 +1,58 @@
 package ca.ubc.cs304.model;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.*;
 
 public class ReservationModel {
+    private final int confNo;
+    private final String vtname;
+    private final String dLicense;
+    private final Timestamp fromDate;
+    private final Timestamp toDate;
+    private final String location;
+
+    public ReservationModel(int confNo, String vtname, String dLicense, Timestamp fromDate, Timestamp toDate, String location) {
+        this.confNo = confNo;
+        this.vtname = vtname;
+        this.dLicense = dLicense;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.location = location;
+    }
+
+    public int getConfNo() {
+        return this.confNo;
+    }
+
+    public String getVtname() {
+        return this.vtname;
+    }
+
+    public String getdLicense() {
+        return this.dLicense;
+    }
+
+    public Timestamp getFromDate() {
+        return this.fromDate;
+    }
+
+
+    public Timestamp getToDate() {
+        return this.toDate;
+    }
+
+    public String getLocation() {return this.location; }
+
+    public String getCityFromLocation(String locn) {
+        switch(locn) {
+            case "SunnyCoast":
+                return "Whistler";
+            case "Bridgeport":
+                return "Richmond";
+            case "Bay":
+                return "Seattle";
+            default:
+                return "Vancouver";
+        }
+    }
 }
